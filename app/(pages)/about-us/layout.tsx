d2e0ce709a300d1fb913c728db8a5f738101f471
Vineeth Silvaraj <vineeth.silvaraj31@gmail.com>
2024-03-02 21:29:43 +0530

    Added about us page

---
+++ app/(pages)/about-us/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Us - ZoomIn Productions",
+  description:
+    "We at ZoomIn are a creative powerhouse. We embrace challenges and adapt to your needs. We strategize and innovate. We create bespoke content that empowers brands, amplifies digital presence, and enriches your brand value.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div>{children}</div>;
+}
---
+++ app/(pages)/about-us/page.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+
+const AboutUsPage: NextPage = () => {
+  return <div>About Us</div>;
+};
+export default AboutUsPage;
