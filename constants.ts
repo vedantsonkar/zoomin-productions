5c6dd391ae4d19d4b3e065328428965349480b8c
Anuj Myageri <anujmyageri147@gmail.com>
2024-03-14 17:27:18 +0530

    Youtube Videos with Thumbnail

--- constants.ts
+++ constants.ts
@@ -19,6 +19,61 @@ const YOUTUBE_VIDEOS = [
     url: 'https://www.youtube.com/embed/U0FjwcImOoQ?si=MzwRTw7mryznQFH6',
     thumbnail: '/images/video_thumbnails/Video_4.png',
   },
+  {
+    name: 'G20 MINISTRY OF EDUCATION FILMS',
+    url: 'https://www.youtube.com/embed/Tzw-z6qmbDg?si=3IB__Ksg_qfvT_19',
+    thumbnail: '/images/video_thumbnails/Video_5.png',
+  },
+  {
+    name: 'CBC Event Films',
+    url: 'https://www.youtube.com/embed/buuaRqqrcHI?si=fBqcKnrzwdRdcUZE',
+    thumbnail: '/images/video_thumbnails/Video_6.png',
+  },
+  {
+    name: 'NTA AWARDS FILMS',
+    url: 'https://www.youtube.com/embed/15E0Y9iJH5A?si=Q5a4zt2EfVivd3sb',
+    thumbnail: '/images/video_thumbnails/Video_7.png',
+  },
+  {
+    name: 'STUDY IN INDIA PORTAL FILM',
+    url: 'https://www.youtube.com/embed/bwnz0FiyM7Q?si=cF9Ufc2NKVQWEH2k',
+    thumbnail: '/images/video_thumbnails/Video_8.png',
+  },
+  {
+    name: 'IIT Indore',
+    url: 'https://www.youtube.com/embed/TmKBSq5M25U?si=xFBE6TGZaEPJqYwh',
+    thumbnail: '/images/video_thumbnails/Video_9.png',
+  },
+  {
+    name: 'IIIT Kota',
+    url: 'https://www.youtube.com/embed/6K1ZwvVy2Jk?si=v2ZNqMJFhoBsET4w',
+    thumbnail: '/images/video_thumbnails/Video_10.png',
+  },
+  {
+    name: 'NEP 2020 EVENT FILM',
+    url: 'https://www.youtube.com/embed/JmDQ9wihoN8?si=c0mgcL7PC9MBco-x',
+    thumbnail: '/images/video_thumbnails/Video_11.png',
+  },
+  {
+    name: 'Ministry of Fisheries',
+    url: 'https://www.youtube.com/embed/qncWv4I4nRk?si=rwXs27CnMPc8ufJJ',
+    thumbnail: '/images/video_thumbnails/Video_12.png',
+  },
+  {
+    name: 'Ministry of Women and Child Development FILMS',
+    url: 'https://www.youtube.com/embed/P_f9BhTwT6s?si=8nr5qDf_7Xw2FbQq',
+    thumbnail: '/images/video_thumbnails/Video_13.png',
+  },
+  {
+    name: 'Mission Chandrayan',
+    url: 'https://www.youtube.com/embed/RzFl8uzY54E?si=JbU28mlCVIsRyWC1',
+    thumbnail: '/images/video_thumbnails/Video_14.png',
+  },
+  {
+    name: 'Prerna Film',
+    url: 'https://www.youtube.com/embed/xW829mJsXu4?si=bfQPw-1wKqApV807',
+    thumbnail: '/images/video_thumbnails/Video_15.png',
+  },
 ];
 
 const LINKS = {
