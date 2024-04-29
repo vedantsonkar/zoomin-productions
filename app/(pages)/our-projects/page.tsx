9636ff9560e002011fbbd1b795ee125d9c1f8110
Vineeth Silvaraj <vineeth.silvaraj31@gmail.com>
2024-04-29 13:09:09 +0530

    content-changes

--- app/(pages)/our-projects/page.tsx
+++ app/(pages)/our-projects/page.tsx
@@ -3,11 +3,12 @@ import VideoCard from '@/components/VideoCard';
 import { YOUTUBE_VIDEOS } from '@/constants';
 import { NextPage } from 'next';
 import OurClientsSection from '@/components/OurClients';
+import ProjectHeader from '@/components/Our Projects/ProjectHeader';
 
-const OurTeamPage: NextPage = () => {
+const OurProjectsPage: NextPage = () => {
   return (
     <div className='mb-20 space-y-16 md:space-y-36 2xl:px-32'>
-      <AboutUsSection />
+      <ProjectHeader />
       <OurClientsSection heading='Organizations that trust us' />
       <h1 className='heading my-10'>Our Projects</h1>
       <div className='grid grid-cols-2 gap-6 px-6 md:grid-cols-4 md:px-20'>
@@ -24,4 +25,4 @@ const OurTeamPage: NextPage = () => {
     </div>
   );
 };
-export default OurTeamPage;
+export default OurProjectsPage;
